3d661714fec53786a1ec18b0389b95eebedea20c
Kio Koh <ewdyoon17@gmail.com>
2023-03-12 23:50:18 +0900

    more changes

--- kiostudios/settings.py
+++ kiostudios/settings.py
@@ -182,17 +182,10 @@ LOGGING = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = BASE_DIR / 'collected-static'
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static/img'),
-#     os.path.join(BASE_DIR, 'static/css'),
-#     os.path.join(BASE_DIR, 'static/js'),
-#     os.path.join(BASE_DIR, 'static/icon'),
-#     ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = BASE_DIR / 'collected-media'
 
 django_heroku.settings(locals())
